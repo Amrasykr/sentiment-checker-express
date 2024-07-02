9ccf7639d8fcd4bdc612eca3d110cf2e75e63cc9
Ammar Asysyakur <114901699+Amrasykr@users.noreply.github.com>
2024-07-02 17:18:16 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -20,8 +20,15 @@ app.use("/api", router);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+// Middleware untuk menangani error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 const startServer = async () => {
   try {
+    console.log("Starting server...");
     await db.authenticate();
     console.log("Database connected...");
     const port = process.env.APP_PORT || 3000;
@@ -30,6 +37,7 @@ const startServer = async () => {
     });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
+    process.exit(1); 
   }
 };
 
