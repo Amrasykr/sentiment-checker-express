d576f0815a6388951ab5428c63d1ed4908c263a5
Ammar Asysyakur <114901699+Amrasykr@users.noreply.github.com>
2024-07-02 17:56:06 +0700

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,24 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from 'mysql2';
 
 dotenv.config();
+
+const sequelizeOptions = {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+};
+
+
+if (sequelizeOptions.dialect === 'mysql') {
+  sequelizeOptions.dialectModule = mysql2;
+}
 
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
+  sequelizeOptions
 );
 
 export default db;
